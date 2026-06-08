4a540883145864523b1bc4795313113bae75da0b
sakshi19-j <jomivalesakshi@gmail.com>
2026-06-08 21:54:40 +0530

    Using Meta test template

--- app/services/reminder_service.py
+++ app/services/reminder_service.py
@@ -320,8 +320,8 @@ async def send_due_reminders_async(
 
             result = await send_template_message(
                 phone         = phone,
-                template_name = "followup_reminde",
-                language      = "en",
+                template_name = "hello_world"
+                language = "en_US"
                 components    = [
                     {
                         "type": "body",
@@ -475,8 +475,8 @@ async def send_single_reminder(
 
     result = await send_template_message(
         phone         = patient.phone_mobile,
-        template_name = "followup_reminde",
-        language      = "en",
+        template_name = "hello_world"
+        language = "en_US"
         components    = [
             {
                 "type": "body",
