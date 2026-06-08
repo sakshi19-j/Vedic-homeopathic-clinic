2ea8ed5ef4be8dab1e3421d1158ed8d808493c17
sakshi19-j <jomivalesakshi@gmail.com>
2026-06-08 21:46:10 +0530

    Fixed template locale to en

--- app/services/reminder_service.py
+++ app/services/reminder_service.py
@@ -321,7 +321,7 @@ async def send_due_reminders_async(
             result = await send_template_message(
                 phone         = phone,
                 template_name = "followup_reminde",
-                language      = "en_US",
+                language      = "en",
                 components    = [
                     {
                         "type": "body",
@@ -476,7 +476,7 @@ async def send_single_reminder(
     result = await send_template_message(
         phone         = patient.phone_mobile,
         template_name = "followup_reminde",
-        language      = "en_US",
+        language      = "en",
         components    = [
             {
                 "type": "body",
